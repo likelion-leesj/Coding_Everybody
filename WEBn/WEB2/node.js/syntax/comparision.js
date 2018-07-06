534903930662eddd604187ec46b387f87579b30d
likelionleesj <leesj@likelion.org>
2018-07-06 06:09:24 +0000

    WEB2 node.js 세번째 커밋:~조건문으로 다른결과출력

---
+++ WEBn/WEB2/node.js/syntax/comparision.js
@@ -0,0 +1,9 @@
+console.log(1+1); //  + 는 이항연산자
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); // true
+console.log(1===2); // false
+
+// == 두개가 같으냐 === 두개가 정확히 같으냐의 차이???? 
