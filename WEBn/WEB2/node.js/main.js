6514c82e6339f284de5bb6d0a78cba5536c9fe44
likelionleesj <leesj@likelion.org>
2018-07-12 06:21:01 +0000

    WEB2 node.js 여덟번째 커밋:수업의 정상까지 도달 하산은 조금 쉬었다가

--- WEBn/WEB2/node.js/main.js
+++ WEBn/WEB2/node.js/main.js
@@ -1,25 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
- 
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-          var template = `
+
+function templateHTML(title,list,body){
+  return `
           <!doctype html>
           <html>
           <head>
@@ -29,45 +13,43 @@ var app = http.createServer(function(request,response){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
+            ${body}
           </body>
           </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
+          `
+}
+function templateLIST(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
  
- 
- 
-      } else {
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
+          var list = templateLIST(filelist);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateLIST(filelist);
+            var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           });
@@ -77,8 +59,5 @@ var app = http.createServer(function(request,response){
       response.writeHead(404);
       response.end('Not found');
     }
- 
- 
- 
-});
+  });
 app.listen(process.env.PORT);
---
+++ WEBn/WEB2/node.js/syntax/function.js
@@ -0,0 +1,21 @@
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+f123();
+console.log("A");
+console.log("D");
+console.log("B");
+f123();
+console.log("E");
+console.log("F");
+console.log("G");
+console.log("H");
+f123();
+console.log("J");
+console.log("K");
+console.log("L");
+f123();
