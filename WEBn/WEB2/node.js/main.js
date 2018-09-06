64b931becf436a09e652b263329203e8b93d7bb9
likelion-leesj <leesj@likelion.org>
2018-09-06 07:16:19 +0000

    WEB2 node.js 열네번째 커밋: 수정 완성과 pm2 추가

--- WEBn/WEB2/node.js/main.js
+++ WEBn/WEB2/node.js/main.js
@@ -77,7 +77,7 @@ var app = http.createServer(function(request,response){
                 <input type="submit">
             </p>
           </form>
-          `,``);
+          `,'');
           response.writeHead(200);
           response.end(template);
         });
@@ -99,7 +99,47 @@ var app = http.createServer(function(request,response){
         })
         // console.log(post.title);
       });
-      
+    } else if(pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateLIST(filelist);
+            var template = templateHTML(title,list,
+            `
+             <form action="/update_process" method="post">
+             <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                  <textarea name="description">${description}</textarea>
+              </p>
+              <p>
+                  <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+    } else if(pathname === '/update_process'){
+      var body ='';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`,description,'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end('success');
+          })
+        });
+      });
     }
     else {
       response.writeHead(404);
